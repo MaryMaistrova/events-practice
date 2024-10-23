b42cd629a77eb7aa2b8dd23b1d46c8395c4b0058
MaryMaistrova <maistrovamariia@gmail.com>
2024-10-23 13:23:24 +0300

    made sorting

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const liName = document.querySelector('li')
 
 liName.addEventListener('click', () => {
-  console.log('clicked')
+
 })
+
+
+  let arrayAllUL = []
+  allUl = document.querySelectorAll('ul')
+
+  for (let i = 1; i < allUl.length; i++) {
+    let current = allUl[i].querySelectorAll('li')[1].innerText;
+    arrayAllUL.push(current)
+  }
+
+  let sortedAZ = arrayAllUL.sort()
+
+  for (let ii = 0; ii < sortedAZ.length; ii++) {
+    for (let i = 1; i < allUl.length; i++) {
+      if (sortedAZ[ii] === allUl[i].querySelectorAll('li')[1].innerText) {
+        sortedTable = document.createElement('section')
+        sortedTable.appendChild(allUl[i])
+        document.body.appendChild(sortedTable)
+      }
+    }
+  }
+
+
